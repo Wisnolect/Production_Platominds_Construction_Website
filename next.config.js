89c0bd878a71834f1d293f1100883fc253eefda8
Pratik <pratik1374@gmail.com>
2023-11-04 20:36:02 +0530

    added netlify configurations

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  // Target must be serverless
+  target: "serverless",
+};
